b6859788b23abf1f94576bfe637aded89981d552
Svensmark <emil.svensmark@gmail.com>
2024-03-30 23:04:57 +0100

    Update play.py

--- src/game/play.py
+++ src/game/play.py
@@ -3,21 +3,19 @@ from game.grid import Grid
 from game.tile import Tile
 from utils.json_reader import File_reader
 
-#Todo - Fjern dette - ryk eventuelt over i en level klasse
-
-# Read JSON
-fr = File_reader()
-levels = fr.read_json('json/maps.json')
-
 
 class Play():
     def __init__(self, pygame: pygame, screen: pygame.Surface):
         self.pygame = pygame
         self.screen = screen
 
-        self.grid = Grid(pygame, screen, levels['level1'], 'stone_tiles')
+        fr = File_reader()
+        self.levels = fr.read_json('json/maps.json')
+
+        self.grid = Grid(pygame, screen, self.levels['level1'], 'stone_tiles')
 
 
     def update(self, screen: pygame.surface, mouse: tuple[int,int], click: bool):
+        #Todo tilføj logik for 
         self.grid.blit_grid(mouse)
         return 'play'
